af18d25d5a040a1acb9469d648b4e4fe2f1a2387
asafarim <asafarim.it@gmail.com>
2024-01-21 23:47:10 +0100

    wip

---
+++ ASP.NET/BlazorWebAssemblyProject/BloggingContext.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorWebAssemblyProject
+{
+    using Azure;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DefaultConnection");
+        }
+    }
+
+}
--- ASP.NET/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ ASP.NET/Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609e7096c7aa696ac46eaf7275be4e8312c1b23f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
