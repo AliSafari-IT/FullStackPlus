51f7882590efdc447b474cdc395dafbd99620e1c
asafarim <asafarim.it@gmail.com>
2024-01-21 15:25:50 +0100

    Add ConsoleApp

---
+++ ConsoleApps/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Cat "implements" the IAnimal interface
+internal class Cat : IAnimal 
+{
+  public void AnimalSound() 
+  {
+    // The body of animalSound() is provided here
+    Console.WriteLine("The Cat says: meow mew"); 
+  }
+}
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Cat myCat = new();  // Create a Cat object
+    myCat.AnimalSound();
+  }
+}
